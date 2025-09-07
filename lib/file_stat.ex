71031a8fe5fb5d83181088ef454735a52432e787
spicychickensauce <spicychickensauce@nope.com>
2025-09-07 11:47:57 +0200

    Init

---
+++ lib/file_stat.ex
@@ -0,0 +1,18 @@
+defmodule FileStat do
+  @moduledoc """
+  Documentation for `FileStat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FileStat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/file_stat_test.exs
@@ -0,0 +1,8 @@
+defmodule FileStatTest do
+  use ExUnit.Case
+  doctest FileStat
+
+  test "greets the world" do
+    assert FileStat.hello() == :world
+  end
+end
